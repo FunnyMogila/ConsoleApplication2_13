a9b5e3c272745668aaf78cef721b7746897c0fa7
FunnyMogila <100155986+FunnyMogila@users.noreply.github.com>
2024-04-18 10:00:46 +0300

    v5

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 #include "VersionControlManager.h"
 
 void printMenu() {
-    std::cout << "Меню:\n";
-    std::cout << "1. Добавить файл\n";
-    std::cout << "2. Добавить версию файла\n";
-    std::cout << "3. Вывести все версии файла\n";
-    std::cout << "4. Вывести конфигурации по дате\n";
-    std::cout << "5. Вывести конфигурацию по номеру версии\n";
-    std::cout << "6. Вывести конфигурации по состоянию\n";
-    std::cout << "0. Выйти\n";
+    std::cout << "Menu:\n";
+    std::cout << "1. Add a file\n";
+    std::cout << "2. Add a file version\n";
+    std::cout << "3. Print all versions of a file\n";
+    std::cout << "4. Print configurations by date\n";
+    std::cout << "5. Print configuration by version number\n";
+    std::cout << "6. Print configurations by state\n";
+    std::cout << "0. Exit\n";
 }
 
 int main() {
@@ -21,16 +21,16 @@ int main() {
     int choice;
     do {
         printMenu();
-        std::cout << "Введите ваш выбор: ";
+        std::cout << "Enter your choice: ";
         std::cin >> choice;
 
         switch (choice) {
         case 1: {
             std::string filename;
-            std::cout << "Введите название файла: ";
+            std::cout << "Enter the filename: ";
             std::cin >> filename;
             manager.addFile(filename);
-            std::cout << "Файл добавлен.\n";
+            std::cout << "File added.\n";
             break;
         }
         case 2: {
@@ -38,56 +38,57 @@ int main() {
             int number;
             bool isFixed;
 
-            std::cout << "Введите название файла: ";
+            std::cout << "Enter the filename: ";
             std::cin >> filename;
-            std::cout << "Введите номер версии: ";
+            std::cout << "Enter the version number: ";
             std::cin >> number;
-            std::cout << "Введите дату версии (ГГГГ-ММ-ДД): ";
+            std::cout << "Enter the version date (YYYY-MM-DD): ";
             std::cin >> date;
-            std::cout << "Фиксированная версия? (1 для да, 0 для нет): ";
+            std::cout << "Is it a fixed version? (1 for yes, 0 for no): ";
             std::cin >> isFixed;
 
             manager.addVersion(filename, FileVersion(number, date, isFixed));
-            std::cout << "Версия добавлена.\n";
+            std::cout << "Version added.\n";
             break;
         }
         case 3: {
             std::string filename;
-            std::cout << "Введите название файла: ";
+            std::cout << "Enter the filename: ";
             std::cin >> filename;
             manager.printAllFileVersions(filename);
             break;
         }
         case 4: {
             std::string date;
-            std::cout << "Введите дату (ГГГГ-ММ-ДД): ";
+            std::cout << "Enter the date (YYYY-MM-DD): ";
             std::cin >> date;
             manager.printConfigurationsByDate(date);
             break;
         }
         case 5: {
             int versionNumber;
-            std::cout << "Введите номер версии: ";
+            std::cout << "Enter the version number: ";
             std::cin >> versionNumber;
             manager.printConfigurationByVersion(versionNumber);
             break;
         }
         case 6: {
             bool isFixed;
-            std::cout << "Введите состояние (1 для фиксированной, 0 для редактируемой): ";
+            std::cout << "Enter the state (1 for fixed, 0 for editing): ";
             std::cin >> isFixed;
             manager.printConfigurationsByState(isFixed);
             break;
         }
         case 0:
-            std::cout << "Выход из программы...\n";
+            std::cout << "Exiting the program...\n";
             break;
         default:
-            std::cout << "Неверный выбор. Пожалуйста, попробуйте снова.\n";
+            std::cout << "Invalid choice. Please try again.\n";
             break;
         }
     } while (choice != 0);
 
     return 0;
 }
+
 
